9fcc27f771f87f01be0a109eebd9295615c9974e
Poojary Shreya Jaya <70750480+PoojaryShreya@users.noreply.github.com>
2021-01-29 11:34:09 +0530

    Create dash.php

---
+++ dash.php
@@ -0,0 +1,24 @@
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="dashstyle.css">
+<meta name="viewport content="width=device-width,initial-scale=1.0">
+<title>Study Material for Engineering Students</title>
+</head>
+<body>
+      <div class="wrapper">
+            <div class="sidebar">
+                <h2>Dashboard</h2>
+                <ul>
+                     <li><a href="search.php">Browse textbook here</a></li>
+                     <li><a href="notes.html">Upload-notes</a></li>
+                     <li><a href="book.php">Upload-books</a></li>
+                     <li><a href="display.php">Download-notes </a></li>
+                     <h3>WELCOME</h3>
+                     
+                </ul>
+              </div>
+         
+       </div>
+</body>
+</html>
